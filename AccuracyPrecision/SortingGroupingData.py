616d1b46bfc499d9eb9144f2de33944a0fb96774
PaulDCross <paul2.cross@live.uwe.ac.uk>
2016-07-20 14:34:00 +0100

    added code to sort and group data

--- AccuracyPrecision/SortingGroupingData.py
+++ AccuracyPrecision/SortingGroupingData.py
@@ -3,5 +3,14 @@ import sys, os
 from operator import itemgetter
 from itertools import groupby
 
-DIR  = os.path.join("..", "..", "Python", "TSP_Testing", "TSP_Testing", "ProgramsData", "TSP_Pictures", "NewPillowRepeatabilityTest", "EE167.5", "358.0mm")
-data = np.load(os.path.join(DIR, "datalineJuly14110.npy"))
+def getKey(item):
+    return item[0]['Z']
+
+DIR   = os.path.join("..", "..", "Python", "TSP_Testing", "TSP_Testing", "ProgramsData", "TSP_Pictures", "NewPillowRepeatabilityTest", "EE167.5", "358.0mm")
+data  = np.load(os.path.join(DIR, "datalineJuly14110.npy"))
+names = data.dtype.names
+
+sortedData  = np.array(sorted(data, key=getKey))                            # Sort data
+groupedData = np.array([list(j) for i,j in groupby(sortedData, key=getKey)])# Group sorted data
+
+
