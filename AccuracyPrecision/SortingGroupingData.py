f3dbbed700dc7d52c2a98c7e940844b420bf1de5
PaulDCross <paul2.cross@live.uwe.ac.uk>
2016-07-20 15:27:53 +0100

    Finished creating a script that computes the MAD in the data

--- AccuracyPrecision/SortingGroupingData.py
+++ AccuracyPrecision/SortingGroupingData.py
@@ -1,7 +1,8 @@
 import numpy as np
-import sys, os
-from operator import itemgetter
+import os
 from itertools import groupby
+import time
+import scipy.io as sio
 
 def getKey(item):
     return item[0]['Z']
@@ -13,4 +14,19 @@ names = data.dtype.names
 sortedData  = np.array(sorted(data, key=getKey))                            # Sort data
 groupedData = np.array([list(j) for i,j in groupby(sortedData, key=getKey)])# Group sorted data
 
+# Calculating the Mean Absoloute Deviation  The average (absolute) distance from the mean
+# group['NewXcoord'][:,i]                   gets all the values across the group for pin i
+# len(group['NewXcoord'][0,:])              gets the number of pins in the image
+# Every entry to the array is a different depth. Every value in every entry is the MAD for the pin at that depth.
+print "Computing..."
+StartTime = time.time()
+madX = np.array([[np.mean(abs(group['NewXcoord'][:,i] - np.mean(group['NewXcoord'][:,i]))) for i in range(len(group['NewXcoord'][0,:]))] for group in groupedData])
+madY = np.array([[np.mean(abs(group['NewYcoord'][:,i] - np.mean(group['NewYcoord'][:,i]))) for i in range(len(group['NewYcoord'][0,:]))] for group in groupedData])
+print time.time() - StartTime
+print "Finished."
 
+print "Saving..."
+StartTime = time.time()
+sio.savemat("madXY.mat", {'madX' : madX, 'madY' : madY})
+print time.time() - StartTime
+print "Finished."
